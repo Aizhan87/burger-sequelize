d2b1968950275c990d0f38d75082a7d8f67c85fb
Aizhan87 <Aizhan87@aizhans-mbp.home>
2019-07-06 21:58:10 -0700

    removed port from server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var exphbs = require("express-handlebars");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 8080;
+// var PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
 var db = require("./models");
@@ -31,8 +31,8 @@ require("./routes/api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync().then(function () {
-    app.listen(PORT, function () {
-        console.log("App listening on PORT " + PORT);
-    });
-});
+// db.sequelize.sync().then(function () {
+//     app.listen(PORT, function () {
+//         console.log("App listening on PORT " + PORT);
+//     });
+// });
